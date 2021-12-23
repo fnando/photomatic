4e9f9992ef82dd09fd49dd89c19da2bcd33b77c1
Nando Vieira <me@fnando.com>
2021-12-22 18:06:24 -0800

    First pass on home design.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def svg_image_tag(name)
+    content_tag :span, class: "svg-#{name}" do
+      File.read(
+        Rails.root.join("app/assets/images").join("#{name}.svg")
+      ).html_safe
+    end
+  end
 end
