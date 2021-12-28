f880fc3a1c6dbaeff3483b279e17f17356d8e22d
Nando Vieira <me@fnando.com>
2021-12-28 15:22:42 -0800

    Apply human_routes changes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,8 @@ Rails.application.routes.draw do
   root "home#index"
 
   route :login do
-    create as: "login"
+    create as: "login", bare: true
+    get :check_inbox
+    get :verify_email
   end
-
-  # Shows a screen saying that the email has been sent.
-  get "check-inbox" => "login#check_inbox", as: :check_inbox
-
-  # Confirms the email that has been sent by validating it against the auth
-  # code.
-  get "verify-email" => "login#verify_email", as: :verify_email
 end
