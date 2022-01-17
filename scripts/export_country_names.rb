69d48823ba6663f5389bce060169d405dddd0cee
Nando Vieira <me@fnando.com>
2022-01-16 23:07:16 -0800

    Fix rubocop violation.

--- scripts/export_country_names.rb
+++ scripts/export_country_names.rb
@@ -12,10 +12,11 @@ I18n.available_locales.each do |locale|
   underscored_locale = locale.to_s.tr("-", "_")
   country_names = JSON.parse(Aitch.get!("https://raw.githubusercontent.com/umpirsky/country-list/master/data/#{underscored_locale}/country.json").data)
 
-  country_names = country_names.each_with_object({}) do |(code_alpha2, name), buffer|
-    code_alpha3 = country_codes[code_alpha2]
-    buffer[code_alpha3] = name
-  end
+  country_names =
+    country_names.each_with_object({}) do |(code_alpha2, name), buffer|
+      code_alpha3 = country_codes[code_alpha2]
+      buffer[code_alpha3] = name
+    end
 
   output_path = Rails.root.join("config/locales/#{locale}/countries.yml")
   FileUtils.mkdir_p(output_path.dirname)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ENV["RAILS_ENV"] ||= "test"
+ENV["RAILS_ENV"] = "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
