25a40e0f14739a44efdaa60ae069cda7afe093a0
Nando Vieira <me@fnando.com>
2022-01-26 13:19:21 -0800

    Don't log to file.

--- config/initializers/development.rb
+++ config/initializers/development.rb
@@ -3,4 +3,8 @@
 Rails.env.on(:development) do
   $stdout.sync = true
   $stderr.sync = true
+
+  logger = ActiveSupport::Logger.new($stdout)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
