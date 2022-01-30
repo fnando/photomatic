9ce9db68cdc46f887e12dc31b8f4ebf23843b235
Nando Vieira <me@fnando.com>
2022-01-30 01:13:08 -0800

    Fix violations.

--- test/policies/can_access_post_test.rb
+++ test/policies/can_access_post_test.rb
@@ -7,22 +7,22 @@ class CanAccessPostTest < ActiveSupport::TestCase
     user = OpenStruct.new(id: 1234)
     post = OpenStruct.new(user_id: user.id, visibility: "private")
 
-    assert CanAccessPost.call(user:, post:)
+    assert CanAccessPost.call(user: user, post: post)
   end
 
   test "is accessible when post is public" do
     user = OpenStruct.new(id: 1234)
     post = OpenStruct.new(user_id: user.id, visibility: "public")
 
-    assert CanAccessPost.call(user:, post:)
-    assert CanAccessPost.call(user: nil, post:)
+    assert CanAccessPost.call(user: user, post: post)
+    assert CanAccessPost.call(user: nil, post: post)
   end
 
   test "is accessible when post is unlisted" do
     user = OpenStruct.new(id: 1234)
     post = OpenStruct.new(user_id: user.id, visibility: "unlisted")
 
-    assert CanAccessPost.call(user:, post:)
-    assert CanAccessPost.call(user: nil, post:)
+    assert CanAccessPost.call(user: user, post: post)
+    assert CanAccessPost.call(user: nil, post: post)
   end
 end
