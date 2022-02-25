9a250f2730273bb97b608dfb5693b0ac8e7c102e
Nando Vieira <me@fnando.com>
2022-02-25 14:46:19 -0800

    Remove hud's http endpoint.

--- test/support/webmock.rb
+++ test/support/webmock.rb
@@ -6,8 +6,7 @@ allowed_sites = [
   "https://chromedriver.storage.googleapis.com",
   "https://github.com/mozilla/geckodriver/releases",
   "https://selenium-release.storage.googleapis.com",
-  "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver",
-  "http://127.0.0.1:32323/hud"
+  "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver"
 ]
 
 WebMock.disable_net_connect!(allow_localhost: true, allow: allowed_sites)
